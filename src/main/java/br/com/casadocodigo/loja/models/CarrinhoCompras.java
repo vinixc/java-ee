2b52f6b03f1ccb1e3350f9740b19bbcb9a5d3e75
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-04-11 17:24:59 -0300

    Criando o backend do Carrinho

---
+++ src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
@@ -0,0 +1,14 @@
+package br.com.casadocodigo.loja.models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CarrinhoCompras {
+	
+	private Set<CarrinhoItem> itens = new HashSet<>();
+	
+	public void add(CarrinhoItem item) {
+		itens.add(item);
+	}
+
+}
