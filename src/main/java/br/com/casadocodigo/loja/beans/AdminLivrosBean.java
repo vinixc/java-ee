d00de3bfc92c8059e8f93dd715b0cf9928145319
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-04-10 21:12:15 -0300

    salvando a imagem no servidor

--- src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,5 +1,6 @@
 package br.com.casadocodigo.loja.beans;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
@@ -10,12 +11,14 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.Part;
 import javax.transaction.Transactional;
 
 import br.com.casadocodigo.loja.daos.AutorDao;
 import br.com.casadocodigo.loja.daos.LivroDao;
 import br.com.casadocodigo.loja.models.Autor;
 import br.com.casadocodigo.loja.models.Livro;
+
 
 @Named
 @RequestScoped
@@ -31,11 +34,13 @@ public class AdminLivrosBean implements Serializable{
 	@Inject
 	private FacesContext context;
 	
+	private Part capaLivro;
+	
 	public AdminLivrosBean() {
 	}
 	
 	@Transactional
-	public String salvar() {
+	public String salvar() throws IOException {
 		if(this.livro.getTitulo().trim().isEmpty()) {
 			FacesMessage msg = new FacesMessage("Titulo não pode ser vazio.");
 			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
@@ -44,6 +49,8 @@ public class AdminLivrosBean implements Serializable{
 		}
 		
 		livroDao.salvar(livro);
+		
+		capaLivro.write("/usr/Sistemas/casadocodigo/livros/" + capaLivro.getSubmittedFileName());
 		
 		context.getExternalContext().getFlash().setKeepMessages(true);
 		context.addMessage(null, new FacesMessage("Livro Cadastrado com Sucesso!"));
@@ -73,6 +80,14 @@ public class AdminLivrosBean implements Serializable{
 			Object value) throws ValidatorException {
 		
 		System.out.println(value);
+	}
+
+	public Part getCapaLivro() {
+		return capaLivro;
+	}
+
+	public void setCapaLivro(Part capaLivro) {
+		this.capaLivro = capaLivro;
 	}
 
 }
