1ead120c1baf63514c4e3446cb3d75d69e1000a5
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-04-11 21:53:40 -0300

    limpando cache

--- src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
+++ src/main/java/br/com/casadocodigo/loja/daos/LivroDao.java
@@ -2,12 +2,26 @@ package br.com.casadocodigo.loja.daos;
 
 import java.util.List;
 
+import javax.persistence.Cache;
+
+import org.hibernate.SessionFactory;
 import org.hibernate.jpa.QueryHints;
 
 import br.com.casadocodigo.loja.models.Livro;
 
 public class LivroDao extends AbstractDao<Livro>{
 	private static final long serialVersionUID = 2885753061340543030L;
+	
+	public void limpaCache() {
+		Cache cache = em.getEntityManagerFactory().getCache();
+		cache.evict(Livro.class, 1l);
+		cache.evictAll();
+		
+		SessionFactory factory = em.getEntityManagerFactory().unwrap(SessionFactory.class);
+		factory.getCache().evictAllRegions();
+		factory.getCache().evictQueryRegion("home");
+		
+	}
 	
 	public Livro buscaLivroPeloId(Integer id) {
 		return em.createNamedQuery(Livro.FIND_LIVRO_BY_ID, Livro.class)
@@ -24,6 +38,7 @@ public class LivroDao extends AbstractDao<Livro>{
 		return em.createNamedQuery(Livro.FIND_FULL_LIVRO, Livro.class)
 				.setMaxResults(5)
 				.setHint(QueryHints.HINT_CACHEABLE, true)
+				.setHint(QueryHints.HINT_CACHE_REGION, "home")
 				.getResultList();
 	}
 	
@@ -31,6 +46,7 @@ public class LivroDao extends AbstractDao<Livro>{
 		return em.createNamedQuery(Livro.FIND_FULL_LIVRO, Livro.class)
 				.setFirstResult(5)
 				.setHint(QueryHints.HINT_CACHEABLE, true)
+				.setHint(QueryHints.HINT_CACHE_REGION, "home")
 				.getResultList();
 	}
 }
