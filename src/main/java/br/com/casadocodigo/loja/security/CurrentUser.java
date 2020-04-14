04c2fd0d41675e20e6ade62a6b1f0cdccd84a9ac
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-04-13 22:56:31 -0300

    Ajustando template e segurança

--- src/main/java/br/com/casadocodigo/loja/security/CurrentUser.java
+++ src/main/java/br/com/casadocodigo/loja/security/CurrentUser.java
@@ -40,6 +40,6 @@ public class CurrentUser {
 	
 	public String logout() {
 		request.getSession().invalidate();
-		return "/livros/lista.xhtml?faces-redirect=true";
+		return "/admin/livros/lista.xhtml?faces-redirect=true";
 	}
 }
